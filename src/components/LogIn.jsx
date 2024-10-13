6d0061890d62ccd5d4be77215e169d81ac31263f
Mimoh123 <mppalikhe@gmail.com>
2024-10-13 12:21:55 +0545

    fixed

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -26,7 +26,7 @@ const LogIn = React.forwardRef(({ changeLoginDivState }, ref) => {
         onClick={() => {
           changeLoginDivState(false);
         }}
-        className='absolute sm:top-10 sm:right-10 top-20 right-36 text-xl hover:cursor-pointer'
+        className='absolute sm:top-10 sm:right-10 top-20 right-44 text-xl hover:cursor-pointer'
         icon={faXmark}
       />
       <h1 className='font-bold text-lg'>Log In</h1>
